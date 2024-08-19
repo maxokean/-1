d0398f295e056848de97875529bf414d1f02c407
maxokean <maxpominenko@gmail.com>
2024-08-19 21:23:44 +0300

    Add files via upload

---
+++ домашка v1.2.py
@@ -0,0 +1,4 @@
+print('1st program:', 9 ** 0.5 * 5)
+print('2nd program:', 9.99>9.98 and 10000<10000.1)
+print('3nd program:', 2+2*2, (2+2)*2, 2+2*2 == (2+2)*2)
+print('4th program:', int(float('123.456')*10)%10)
