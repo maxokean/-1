7943ad9c8a3ab165b2a95ef7014547b4c4bee078
maxokean <maxpominenko@gmail.com>
2024-09-06 16:59:37 +0300

    Add files via upload

---
+++ enter the matrix.py
@@ -0,0 +1,21 @@
+# n = int(input('n='))
+# m = int(input ('m='))
+# value = int(input ('value='))
+
+
+def get_matrix(n, m, value):
+    matrix = []
+    empty = []
+    for n in range (n):
+        matrix.append(empty)
+        for m in range(m):
+            (empty.append(value))
+
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
